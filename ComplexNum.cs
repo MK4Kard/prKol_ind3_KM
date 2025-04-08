980f5b550ad12950e106a6fcd394bf58b1765e79
MK4Kard <misha212032@gmail.com>
2025-04-08 18:27:21 +0500

    Add files via upload

---
+++ ComplexNum.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace prKol_ind3_1_KM
+{
+    class ComplexNum
+    {       
+        public string Add(string n, string m)
+        {
+            var firstN = ComplexParts(n);
+            var firstM = ComplexParts(m);
+
+            int a = Convert.ToInt32(firstN[0]) + Convert.ToInt32(firstM[0]);
+            int b = Convert.ToInt32(firstN[1]) + Convert.ToInt32(firstM[1]);
+
+            string sum = Answer(a, b);
+
+            return sum;
+        }
+        public string Sub(string n, string m)
+        {
+            var firstN = ComplexParts(n);
+            var firstM = ComplexParts(m);
+
+            int a = Convert.ToInt32(firstN[0]) - Convert.ToInt32(firstM[0]);
+            int b = Convert.ToInt32(firstN[1]) - Convert.ToInt32(firstM[1]);
+
+            string sum = Answer(a, b);
+
+            return sum;
+        }
+        public string Mult(string n, string m)
+        {
+            var firstN = ComplexParts(n);
+            var firstM = ComplexParts(m);
+
+            int a1 = Convert.ToInt32(firstN[0]);
+            int a2 = Convert.ToInt32(firstM[0]);
+            int b1 = Convert.ToInt32(firstN[1]);
+            int b2 = Convert.ToInt32(firstM[1]);
+
+            int a = a1 * a2 - b1 * b2;
+            int b = a1 * b2 + b1 * a2;
+
+            string sum = Answer(a, b);
+
+            return sum;
+        }
+
+        private ArrayList ComplexParts(string complexNum)
+        {
+            var parts = new ArrayList();
+
+            int firstPlus = complexNum.IndexOf("+");
+            if (firstPlus == -1)
+                firstPlus = complexNum.LastIndexOf("-");
+
+            string a = complexNum.Substring(0, firstPlus).Trim();
+            string b = complexNum.Substring(firstPlus).Trim().Replace("i", "");
+            if (b == "+")
+            {
+                parts.Add(Convert.ToInt32(a));
+                parts.Add(1);
+            }
+            else if (b == "-")
+            {
+                parts.Add(Convert.ToInt32(a));
+                parts.Add(-1);
+            }
+            else
+            {
+                parts.Add(Convert.ToInt32(a));
+                parts.Add(Convert.ToInt32(b));
+            }
+
+            return parts;
+        }
+        private string Answer(int a, int b)
+        {
+            var parts = new[] { a.ToString() }.Concat(b > 1 ? new[] { $"+{b}i" } :
+                b < -1 ? new[] { $"{b}i" } :
+                b == 0 ? new[] { $"" } :
+                b == 1 ? new[] { $"+i" } :
+                b == -1 ? new[] { $"-i" } :
+                new string[] { });
+
+            return string.Join("", parts);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace prKol_ind3_1_KM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNum z = new ComplexNum();
+
+            ArrayList nums = new ArrayList();
+
+            Console.WriteLine("Введите название файла с числами");
+            string name = Console.ReadLine();
+
+            if (File.Exists(name))
+            {
+                string[] z_num = File.ReadAllLines(name);
+
+                foreach (string num in z_num)
+                {
+                    nums.Add(num);
+                }
+
+                if (nums.Count % 2 == 0)
+                {
+                    int i = 0;
+
+                    while (i < nums.Count)
+                    {
+                        string z1 = nums[i].ToString();
+                        string z2 = nums[i + 1].ToString();
+
+                        Console.WriteLine($"Комплексные числа:\n{z1}\n{z2}");
+
+                        if (ValComplexNum(z1) && ValComplexNum(z2))
+                        {
+                            z1 = z1.Replace(" ", "");
+                            z2 = z2.Replace(" ", "");
+
+                            Console.WriteLine("Какое действие совершить\n1 - сложение\n2 - вычитание\n3 - умножение");
+                            int n = Convert.ToInt32(Console.ReadLine());
+
+                            switch (n)
+                            {
+                                case 1:
+                                    Console.WriteLine(z.Add(z1, z2));
+                                    break;
+                                case 2:
+                                    Console.WriteLine(z.Sub(z1, z2));
+                                    break;
+                                case 3:
+                                    Console.WriteLine(z.Mult(z1, z2));
+                                    break;
+                                default:
+                                    Console.WriteLine("Неверный вариант выбора");
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Комплексное число записано неправильно");
+                            break;
+                        }
+
+                        i += 2;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Чисел должно быть ровно");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такого файла не существует");
+            }
+
+            Console.ReadLine();
+        }
+        static bool ValComplexNum(string z)
+        {
+            string pattern = @"^([-+]?d*.?d*)s*([-+]s*d*.?d*)i$";
+
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(z.Replace(" ", ""));
+        }
+    }
+}
